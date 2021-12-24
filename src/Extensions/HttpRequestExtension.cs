4ca546a71be4d81ede130b37e922c222dfe5ae5b
venyowong <venyowong@163.com>
2021-12-24 18:47:15 +0800

    增加报错日志信息

---
+++ src/Extensions/HttpRequestExtension.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTemplate.Extensions
+{
+    public static class HttpRequestExtension
+    {
+        public static async Task<string> ReadBody(this HttpRequest request)
+        {
+            if (request == null)
+            {
+                return string.Empty;
+            }
+
+            try
+            {
+                using (var reader = new StreamReader(request.Body, Encoding.UTF8, false, -1, true))
+                {
+                    var result = await reader.ReadToEndAsync();
+                    request.Body.Position = 0;
+                    return result;
+                }
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        public static string GetAbsoluteUrl(this HttpRequest request) => $"{request.Scheme}://{request.Host}{request.PathBase}{request.Path}{request.QueryString}";
+    }
+}
--- src/Middlewares/LogMiddleware.cs
+++ src/Middlewares/LogMiddleware.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using ProjectTemplate.Extensions;
 using Serilog;
 using Serilog.Context;
 
@@ -19,15 +20,21 @@ namespace ProjectTemplate.Middlewares
         {
             using (LogContext.PushProperty("TraceId", context.TraceIdentifier))
             {
+                string body = string.Empty;
                 try
                 {
+                    context.Request.EnableBuffering();
+                    if (context.Request.Method == "POST")
+                    {
+                        body = await context.Request.ReadBody();
+                    }
                     await this.next(context);
                 }
                 catch (Exception e)
                 {
                     context.Response.StatusCode = StatusCodes.Status500InternalServerError;
                     context.Response.ContentType = "application/json";
-                    Log.Error(e, "an error occure");
+                    Log.Error(e, $"an error occure, url: {context.Request.GetAbsoluteUrl()} body: {body}");
                     await context.Response.WriteAsync("an error occure");
                 }
             }
